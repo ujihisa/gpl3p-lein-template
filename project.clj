cf895f800f41433af96e16e40b482270f84661f8
ujihisa <ujihisa@gmail.com>
2013-12-08 17:09:55 -0800

    gpl3p

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject gpl3p/lein-template "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "GNU GPL v3+"
+            :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
+  :eval-in-leiningen true)
---
+++ src/leiningen/new/gpl3p.clj
@@ -0,0 +1,12 @@
+(ns leiningen.new.gpl3p
+  (:use [leiningen.new.templates :only [renderer name-to-path ->files]]))
+
+(def render (renderer "gpl3p"))
+
+(defn gpl3p
+  "FIXME: write documentation"
+  [name]
+  (let [data {:name name
+              :sanitized (name-to-path name)}]
+    (->files data
+             ["src/{{sanitized}}/foo.clj" (render "foo.clj" data)])))
