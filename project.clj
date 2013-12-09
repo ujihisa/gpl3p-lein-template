85d9062b9df3dbd4ba642e1e2410cd1b74cca7e2
ujihisa <ujihisa@gmail.com>
2013-12-08 18:25:09 -0800

    description for clojars.org

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject gpl3p/lein-template "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject gpl3p/lein-template "0.1.1-SNAPSHOT"
+  :description "Basically same to the default `lein new` except for the license. GPLv3+."
+  :url "https://github.com/ujihisa/gpl3p-lein-template"
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :eval-in-leiningen true)
