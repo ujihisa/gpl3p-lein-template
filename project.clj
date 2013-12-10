71d53ce13a639c2217afdab808b374f905155be8
ujihisa <ujihisa@gmail.com>
2013-12-09 22:43:17 -0800

    0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gpl3p/lein-template "0.1.1-SNAPSHOT"
+(defproject gpl3p/lein-template "0.1.1"
   :description "Basically same to the default `lein new` except for the license. GPLv3+."
   :url "https://github.com/ujihisa/gpl3p-lein-template"
   :license {:name "GNU GPL v3+"
