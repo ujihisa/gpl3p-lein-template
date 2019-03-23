42f7826ba8a11ea2326c86bd62ed1ef012a90ea0
Leandro Doctors <ldoctors@gmail.com>
2019-03-23 09:39:45 -0300

    Update project.clj (from Leiningen)

--- src/leiningen/new/gpl3p/project.clj
+++ src/leiningen/new/gpl3p/project.clj
@@ -1,8 +1,9 @@
-(defproject {{name}} "0.1.0-SNAPSHOT"
-  :description "<`1:FIXME: write description`>"
+(defproject {{raw-name}} "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "GNU GPL v3+"
+  :license {:name "GNU GPL v3+, with Clojure exception"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main {{name}}.core
-  :eval-in-leiningen true)
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :main ^:skip-aot {{namespace}}
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
